e1c38a00160c90b951b85f65cd7f29257b330120
AflahPS <aflahps7@gmail.com>
2022-08-04 23:29:58 +0530

    font core dump error

--- Font-Pattern/digital_font.c
+++ Font-Pattern/digital_font.c
@@ -4,9 +4,9 @@
 int main(void)
 {
 
-    int input_num = 10, num_length, size = 1, i, j;
-    int digits[100],temp[100],mod;
-    
+    int input_num = 10, num_length, size = 1, i, j, k;
+    int digits[100], temp[100], mod;
+
     printf("Please enter size of the font: \n");
     scanf("%d", &size);
 
@@ -28,7 +28,6 @@ int main(void)
             if (input_num > 0)
             {
                 mod = input_num % 10;
-                printf("%d\n", mod);
                 digits[i] = mod;
                 input_num = (input_num - mod) / 10;
                 num_length++;
@@ -40,7 +39,7 @@ int main(void)
     {
         temp[i] = digits[num_length - 1 - i];
     }
-    printf("Inside array\n");
+
     for (i = 0; i < num_length; i++)
     {
         digits[i] = temp[i];
@@ -49,213 +48,126 @@ int main(void)
     /* clear screen */
     system("clear");
 
-    /* number 1 */
-    
-    if (input_num == 1)
+    for (i = 1; i <= height; i++)
     {
-        for (i = 1; i <= height; i++)
+        for (j = 1; j <= width; j++)
         {
-            for (j = 1; j <= width; j++)
+            for (k = 0; k < num_length; k++)
             {
-                if (j == mid)
-                {
-                    printf(" %d ", input_num);
-                }
-                else
-                {
-                    printf("   ");
-                }
-            }
-            printf("\n");
-        }
-    }
 
-    /* number 2*/
-    if (input_num == 2)
-    {
-        for (i = 1; i <= height; i++)
-        {
-            for (j = 1; j <= width; j++)
-            {
-                if (i == 1 || i == width || i == height)
+                if (digits[k] == 1)
                 {
-                    printf(" %d ", input_num);
+                    if (j == mid)
+                    {
+                        printf(" * ");
+                    }
+                    else
+                    {
+                        printf("   ");
+                    }
                 }
-                else if (i > 1 && i < width && j == width)
+                else if (digits[k] == 2)
                 {
-                    printf(" %d ", input_num);
+                    if (i == 1 || i == width || i == height || i > 1 && i < width && j == width || i > width && i < height && j == 1)
+                    {
+                        printf(" * ");
+                    }
+                    else
+                    {
+                        printf("   ");
+                    }
                 }
-                else if (i > width && i < height && j == 1)
+                else if (digits[k] == 3)
                 {
-                    printf(" %d ", input_num);
+                    if (i == 1 || i == width || i == height || j == width)
+                    {
+                        printf(" * ");
+                    }
+                    else
+                    {
+                        printf("   ");
+                    }
                 }
-                else
+                else if (digits[k] == 4)
                 {
-                    printf("   ");
+                    if (i >= 1 && i < width && j == width - i + 1 || i == width || i > width && i <= height && j == width)
+                    {
+                        printf(" * ");
+                    }
+                    else
+                    {
+                        printf("   ");
+                    }
                 }
-            }
-            printf("\n");
-        }
-    }
-
-    /*number 3*/
-    if (input_num == 3)
-    {
-        for (i = 1; i <= height; i++)
-        {
-            for (j = 1; j <= width; j++)
-            {
-                if (i == 1 || i == width || i == height || j == width)
+                else if (digits[k] == 5)
                 {
-                    printf(" %d ", input_num);
+                    if (i == 1 || i == width || i == height || i >= 1 && i < width && j == 1 || i > width && i <= height && j == width)
+                    {
+                        printf(" * ");
+                    }
+                    else
+                    {
+                        printf("   ");
+                    }
                 }
-                else
+                else if (digits[k] == 6)
                 {
-                    printf("   ");
+                    if (i == 1 || i == width || i == height || j == 1 || i > width && i <= height && j == width)
+                    {
+                        printf(" * ");
+                    }
+                    else
+                    {
+                        printf("   ");
+                    }
                 }
-            }
-            printf("\n");
-        }
-    }
-
-    /*number 4*/
-    if (input_num == 4)
-    {
-        for (i = 1; i <= height; i++)
-        {
-            for (j = 1; j <= width; j++)
-            {
-                if (i >= 1 && i < width && j == width-i+1|| i == width || i > width && i <= height && j == width)
+                else if (digits[k] == 7)
                 {
-                    printf(" %d ", input_num);
+                    if (i == 1 || j == width)
+                    {
+                        printf(" * ");
+                    }
+                    else
+                    {
+                        printf("   ");
+                    }
                 }
-                else
+                else if (digits[k] == 8)
                 {
-                    printf("   ");
+                    if (i == 1 || i == width || i == height || j == 1 || j == width)
+                    {
+                        printf(" * ");
+                    }
+                    else
+                    {
+                        printf("   ");
+                    }
                 }
-            }
-            printf("\n");
-        }
-    }
-
-    /*number 5*/
-    if (input_num == 5)
-    {
-        for (i = 1; i <= height; i++)
-        {
-            for (j = 1; j <= width; j++)
-            {
-                if (i == 1 || i == width || i == height || i >= 1 && i < width && j == 1 || i > width && i <= height && j == width)
+                else if (digits[k] == 9)
                 {
-                    printf(" %d ", input_num);
+                    if (i == 1 || i == width || i == height || j == width || i >= 1 && i < width && j == 1)
+                    {
+                        printf(" * ");
+                    }
+                    else
+                    {
+                        printf("   ");
+                    }
                 }
-                else
+                else if (digits[k] == 0)
                 {
-                    printf("   ");
-                }
-            }
-            printf("\n");
-        }
-    }
-
-    /*number 6*/
-    if (input_num == 6)
-    {
-        for (i = 1; i <= height; i++)
-        {
-            for (j = 1; j <= width; j++)
-            {
-                if (i == 1 || i == width || i == height || j == 1 || i > width && i <= height && j == width)
-                {
-                    printf(" %d ", input_num);
-                }
-                else
-                {
-                    printf("   ");
-                }
-            }
-            printf("\n");
-        }
-    }
-
-    /*number 7*/
-    if (input_num == 7)
-    {
-        for (i = 1; i <= height; i++)
-        {
-            for (j = 1; j <= width; j++)
-            {
-                if (i == 1 || j == width)
-                {
-                    printf(" %d ", input_num);
-                }
-                else
-                {
-                    printf("   ");
+                    if (i == 1 || i == height || j == width || j == 1)
+                    {
+                        printf(" * ");
+                    }
+                    else
+                    {
+                        printf("   ");
+                    }
                 }
             }
-            printf("\n");
         }
-    }
-
-    /*number 8*/
-    if (input_num == 8)
-    {
-        for (i = 1; i <= height; i++)
-        {
-            for (j = 1; j <= width; j++)
-            {
-                if (i == 1 || i == width || i == height || j == 1 || j == width)
-                {
-                    printf(" %d ", input_num);
-                }
-                else
-                {
-                    printf("   ");
-                }
-            }
-            printf("\n");
-        }
-    }
-
-    /*number 9*/
-    if (input_num == 9)
-    {
-        for (i = 1; i <= height; i++)
-        {
-            for (j = 1; j <= width; j++)
-            {
-                if (i == 1 || i == width || i == height || j == width || i >= 1 && i < width && j == 1)
-                {
-                    printf(" %d ", input_num);
-                }
-                else
-                {
-                    printf("   ");
-                }
-            }
-            printf("\n");
-        }
-    }
-
-    /*number 0*/
-    if (input_num == 0)
-    {
-        for (i = 1; i <= height; i++)
-        {
-            for (j = 1; j <= width; j++)
-            {
-                if (i == 1 || i == height || j == width || j == 1)
-                {
-                    printf(" %d ", input_num);
-                }
-                else
-                {
-                    printf("   ");
-                }
-            }
-            printf("\n");
-        }
+        printf("\n");
     }
 
     return 0;
